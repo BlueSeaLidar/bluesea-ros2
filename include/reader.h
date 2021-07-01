a42c02a9057355dfe1b2751c2e902803ac6228f5
BlueSeaLidar <60593455+BlueSeaLidar@users.noreply.github.com>
2021-07-01 17:34:44 +0800

    stop reader

--- include/reader.h
+++ include/reader.h
@@ -11,16 +11,20 @@ void PublishData(HPublish, int, RawData**);
 
 
 HReader StartUartReader(const char* port, int baudrate, int* rate_list, HParser, HPublish);
+void StopUartReader(HReader);
+
 bool SendUartCmd(HReader, int len, char*);
 
 
 HReader StartUDPReader(const char* lidar_ip, unsigned short lidar_port, 
 		const char* group_ip, unsigned short listen_port, 
 		HParser hParser, HPublish hPub);
+void StopUDPReader(HReader);
 
 bool SendUdpCmd(HReader hr, int len, char* cmd);
 
 HReader StartTCPReader(const char* lidar_ip, unsigned short lidar_port, HParser hParser, HPublish hPub);
+void StopTCPReader(HReader);
 
 bool SendTcpCmd(HReader hr, int len, char* cmd);
 
