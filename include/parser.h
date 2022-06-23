944999a0acf1f9d2d1e686a9a0af3e6e07f25b55
BlueSeaLidar <60593455+BlueSeaLidar@users.noreply.github.com>
2022-06-23 12:41:57 +0800

    Add files via upload

--- include/parser.h
+++ include/parser.h
@@ -7,7 +7,10 @@
 #define DF_SMOOTHED		0x0008
 #define DF_FAN_90		0x0020
 #define DF_WITH_RESAMPLE	0X0080
+#define DF_MOTOR_REVERSE	0x0100
 #define DF_WITH_UUID		0X1000
+
+#define EF_ENABLE_ALARM_MSG	0X10000
 
 #define MAX_FANS 120
 
@@ -16,6 +19,8 @@
 //#define ANYONE 0x1234abcd
 #define ANYONE -1
 
+
+#include <arpa/inet.h>
 struct DataPoint
 {
 	uint16_t idx;
@@ -38,6 +43,7 @@ struct RawData
 	// short ros_angle;	// 0.1 degree
 	DataPoint points[MAX_POINTS];
 	uint32_t ts[2];
+	uint8_t	counterclockwise;
 
 	//unsigned short distance[1000];
 	//unsigned char confidence[1000];
@@ -45,6 +51,19 @@ struct RawData
 };
 
 typedef void* HParser;
+typedef void* HReader;
+typedef void* HPublish;
+
+
+struct LidarNode 
+{
+	HParser hParser;
+	HPublish hPublish;
+	char ip[30];
+	int port;
+	in_addr_t s_addr;
+};
+
 
 HParser ParserOpen(int raw_bytes, 
 		uint32_t device_ability,
@@ -61,7 +80,7 @@ bool ParserScript(HParser, Script, void*);
 
 int ParserClose(HParser);
 int ParserRunStream(HParser, int len, unsigned char* buf, RawData* fans[]);
-int ParserRun(HParser, int len, unsigned char* buf, RawData* fans[]);
+int ParserRun(LidarNode, int len, unsigned char* buf, RawData* fans[]);
 
 void SetTimeStamp(RawData*); 
 
--- include/reader.h
+++ include/reader.h
@@ -3,29 +3,41 @@
 
 #include "parser.h"
 
-typedef void* HReader;
-typedef void* HPublish;
 
 
+
+#define MAX_LIDARS 8
+
+
+struct LidarInfo {
+	HParser parser;
+	HPublish pub;
+	char lidar_ip[32];
+	int lidar_port;
+};
 void PublishData(HPublish, int, RawData**);
 
-
 HReader StartUartReader(const char* port, int baudrate, int* rate_list, HParser, HPublish);
-void StopUartReader(HReader);
-
 bool SendUartCmd(HReader, int len, char*);
 
 
-HReader StartUDPReader(const char* lidar_ip, unsigned short lidar_port, 
-		const char* group_ip, unsigned short listen_port, 
+HReader StartUDPReader(const char* lidar_ip, unsigned short lidar_port, unsigned short listen_port, 
+		bool is_group_listener, const char* group_ip, 
 		HParser hParser, HPublish hPub);
-void StopUDPReader(HReader);
 
-bool SendUdpCmd(HReader hr, int len, char* cmd);
+HReader StartUDPReader( unsigned short listen_port, bool is_group_listener, const char* group_ip,
+	int lidar_count, const LidarInfo* lidars);
+
+
+bool SendUdpCmd(HReader hr, int id, int len, char* cmd);
 
 HReader StartTCPReader(const char* lidar_ip, unsigned short lidar_port, HParser hParser, HPublish hPub);
-void StopTCPReader(HReader);
 
 bool SendTcpCmd(HReader hr, int len, char* cmd);
+
+void StopUartReader(HReader hr);
+void StopUDPReader(HReader hr);
+void StopTCPReader(HReader hr);
+
 
 #endif
